86a6d51957b5b5ca438f5dc80d152babf6703746
Leonardo <liogothicvampire@gmail.com>
2018-08-24 17:03:40 -0500

    Building an App with GGradle

---
+++ BuildingSpringBootInitialzrGradle/src/main/java/com/lio/bouchan/BuildingSpringBootInitialzrGradle/BuildingSpringBootInitialzrGradleApplication.java
@@ -0,0 +1,12 @@
+package com.lio.bouchan.BuildingSpringBootInitialzrGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingSpringBootInitialzrGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingSpringBootInitialzrGradleApplication.class, args);
+	}
+}
